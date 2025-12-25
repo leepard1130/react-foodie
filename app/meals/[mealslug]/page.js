772c03654b03c5ea2cf7d563c84096da60218a57
Eric Lee <39612462+leepard1130@users.noreply.github.com>
2025-12-25 12:44:08 +1000

    fetching meal details

--- app/meals/[mealslug]/page.js
+++ app/meals/[mealslug]/page.js
@@ -1,3 +1,33 @@
-export default function MealDetailsPage() {
-    return <h1>Meal Details Page</h1>
+import classes from './page.module.css';
+import Image from 'next/image';
+import { getMealBySlug } from '@/lib/meals';
+
+export default function MealDetailsPage({params}) {
+    const meal =  getMealBySlug(params.mealslug);
+    meal.instructions = meal.instructions.replace(/\n/g, '<br/>');// Convert newlines to <br/> tags
+
+    return (
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} alt={meal.title} fill />
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                        Created by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+                    </p>
+                    <p className={classes.summary}>{meal.summary}</p>
+                </div>
+            </header>
+            <main>
+                <p
+                    className={classes.instructions}
+                    dangerouslySetInnerHTML={{
+                        __html: meal.instructions,
+                    }}
+                ></p>
+            </main>
+        </>
+    );
 }
