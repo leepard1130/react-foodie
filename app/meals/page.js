e599f2ab66a2ce3fa50745670d047339c03eefef
Eric Lee <39612462+leepard1130@users.noreply.github.com>
2025-12-25 11:30:38 +1000

    adding Suspense component to Meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,9 +2,14 @@ import { getMeals } from '@/lib/meals';
 import MealsGrid from '../components/meals/meals-grid';
 import classes from './page.module.css';
 import Link from 'next/link';
+import { Suspense } from 'react';
 
-export default async function MealsPage() {
+async function Meals() {
     const meals = await getMeals();
+
+    return <MealsGrid meals={meals} />;
+}
+export default function MealsPage() {
 
     return (
         <>
@@ -23,7 +28,9 @@ export default async function MealsPage() {
                 </p>
             </header>
             <main className={classes.main}>
-                <MealsGrid meals={ meals } />
+                <Suspense fallback={ <p className={classes.loading}>Loading Page Now...</p>}>
+                    <Meals />
+                </Suspense>
             </main>     
         </>
         );
