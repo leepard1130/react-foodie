875878af776b93f436b4dcf619947f0cf7847391
Eric Lee <39612462+leepard1130@users.noreply.github.com>
2025-12-27 22:39:45 +1000

    using useFormState to manage from states

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,11 +1,13 @@
 'use client';
+import { useFormState } from 'react-dom';
 import classes from './page.module.css';
 import ImagePicker from '@/app/components/meals/image-picker';
 import { shareMeal } from '@/lib/action';
 import MealsFormSubmit from '@/app/components/meals/meals-form-submit';
 
 export default function ShareMealPage() {
-    
+    const [state, formAction] = useFormState(shareMeal, {message: null});
+
     return (
         <>
         <header className={classes.header}>
@@ -15,7 +17,7 @@ export default function ShareMealPage() {
             <p>Or any other meal you feel needs sharing!</p>
         </header>
         <main className={classes.main}>
-            <form className={classes.form} action={shareMeal}>
+            <form className={classes.form} action={formAction}>
             <div className={classes.row}>
                 <p>
                 <label htmlFor="name">Your name</label>
@@ -43,7 +45,8 @@ export default function ShareMealPage() {
                 required
                 ></textarea>
             </p>
-            <ImagePicker name="image" label="Your image" ß/>
+            <ImagePicker name="image" label="Your image"/>
+            {state.message && <p>{state.message}</p>}
             <p className={classes.actions}>
                 <MealsFormSubmit />
             </p>
